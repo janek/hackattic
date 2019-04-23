82bf47208af1aca5df48d424f6ff73f1dbca139b
Janek Szynal <jan.szynal@gmail.com>
2019-04-23 02:36:38 +0200

    Binary representation for image; counting vertical line lenghts. Added example png.

--- reading-qr/reading-qr.py
+++ reading-qr/reading-qr.py
@@ -2,25 +2,51 @@ import requests
 import json 
 from PIL import Image                                                                                
 import numpy as np
+import pandas as pd
 from io import BytesIO
 import matplotlib.pyplot as plt
-import cv2
 
-# https://matplotlib.org/users/image_tutorial.html
+# url = 'https://hackattic.com/challenges/reading_qr/problem?access_token=baa64683b7798b3f'
 
-
-url = 'https://hackattic.com/challenges/reading_qr/problem?access_token=baa64683b7798b3f'
+# Get the task image: a QR code, placed and rotated at random on a 600x600 canvas.
+# Read the png file, get to a single channel numpy array of zeroes and ones .
 
 response = requests.get(url)
 img_url = response.json()['image_url']
 img_data = requests.get(img_url)
 img_png = Image.open(BytesIO(img_data.content))
-img_rgba = np.array(img_png)
 
-r = img_rgba[:,:,0]
-(r == 255).sum()
-(r == 0).sum()
-(r == 255).sum() + (r == 0).sum() - len(r.ravel())
+
+img_rgba = np.array(img_png)
+img_bw = img_rgba[:,:,0]
+
+img_bw[img_bw <= 120] = 1 
+img_bw[img_bw > 120] = 0
+
+plt.imshow(img_bw)
+img = img_bw
+
+# Count length of verticals as a way of determining rotation
+verticals = []
+for c in range(img.shape[1]):
+    first_pixel_index = 0
+    last_pixel_index = 0
+    for r in range(img.shape[0]):
+        if img[r,c] == 1:
+            if first_pixel_index == 0:
+                first_pixel_index = r
+            last_pixel_index = r
+    vertical_length = last_pixel_index - first_pixel_index
+    verticals.append(vertical_length)
+    
+
+verticals
+
+
+
+
+
+
 
 
 
