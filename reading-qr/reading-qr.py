2cd3ed1e69fadb6a0ed0cbfbc165ad15e954b02b
Janek Szynal <jan.szynal@gmail.com>
2019-04-28 00:23:45 +0200

    Decoding QR with zbarlight

--- reading-qr/reading-qr.py
+++ reading-qr/reading-qr.py
@@ -2,11 +2,10 @@ import requests
 import json 
 from PIL import Image                                                                                
 import numpy as np
-import pandas as pd
-import seaborn as sns
 from io import BytesIO
 import scipy.ndimage.interpolation as interpolation
 import matplotlib.pyplot as plt
+import zbarlight #NOTE: might need more than pip install. see https://github.com/Polyconseil/zbarlight/
 
 url = 'https://hackattic.com/challenges/reading_qr/problem?access_token=baa64683b7798b3f'
 
@@ -17,7 +16,6 @@ img_url = response.json()['image_url']
 img_data = requests.get(img_url)
 img_png = Image.open(BytesIO(img_data.content))
 
-
 # Read the png file, get to a single channel numpy array of zeroes and ones.
 plt.imshow(img_png)
 img_rgba = np.array(img_png)
@@ -25,17 +23,18 @@ img = img_rgba[:,:,0]
 plt.imshow(img)
 
 # Trim whitespace from the canvas
-short = img[~np.all(img == 255, axis=1)]
-small = short.T[~np.all(short == 255, axis=0)].T
-img = small
-
+def clip_whitespace(img):
+    short = img[~np.all(img == 255, axis=1)]
+    small = short.T[~np.all(short == 255, axis=0)].T
+    return small
+img = clip_whitespace(img)
 
 # To find a reasonable position for the code:
-# rotate a bit, trim whitespace, check width 
-# minimum width is a reasonable rotation
+# 1. rotate a bit, trim whitespace, check width 
+# 2, minimum width is a reasonable rotation (nice_angle)
 
 #NOTE: this is iterative search, should at least be binary to be efficient
-#NOTE: this is exact to 1deg, could be a bit more (esp. if binary search)
+#NOTE: this is exact to 1deg, we could consider making it more (or less) exact
 size_sums = []
 for angle in range(360):
     rotated = interpolation.rotate(img, angle, cval=255)
@@ -43,6 +42,22 @@ for angle in range(360):
     size_sums.append(clipped.shape[0])
 
 nice_angle = np.array(size_sums).argmin()
-rotated = interpolation.rotate(img, 58, cval=255)
+rotated = interpolation.rotate(img, nice_angle, cval=255)
 clipped = clip_whitespace(rotated)
 plt.imshow(clipped)
+qr = clipped
+
+
+# Convert to a format known to the qr scanner and decode
+#   NOTE: the scanner is unfortunately too powerful and works for any orientation
+#   (in fact, it even works for the original image).
+image = Image.fromarray(qr)
+codes = zbarlight.scan_codes(['qrcode'], image)
+
+
+print(codes)
+
+# One of four possible 90deg rotations is the correct one
+# XXX: detect the right one once qr decoder is switched to a weaker one
+for angle in [90, 180, 270]:
+   rotated = interpolation.rotate(qr, angle)
