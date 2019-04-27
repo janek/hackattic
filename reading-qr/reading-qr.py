5ae5451effc146af6f36a2e93615b5d8e21c2c4e
Janek Szynal <jan.szynal@gmail.com>
2019-04-27 20:44:39 +0200

    Extracting QR from image with iterative rotation-search

--- reading-qr/reading-qr.py
+++ reading-qr/reading-qr.py
@@ -3,55 +3,46 @@ import json
 from PIL import Image                                                                                
 import numpy as np
 import pandas as pd
+import seaborn as sns
 from io import BytesIO
+import scipy.ndimage.interpolation as interpolation
 import matplotlib.pyplot as plt
 
 url = 'https://hackattic.com/challenges/reading_qr/problem?access_token=baa64683b7798b3f'
 
-# Get the task image: a QR code, placed and rotated at random on a 600x600 canvas.
-# Read the png file, get to a single channel numpy array of zeroes and ones .
-
+# Get the task image: a QR code, 
+# placed and rotated at random on a 600x600 canvas.
 response = requests.get(url)
 img_url = response.json()['image_url']
 img_data = requests.get(img_url)
 img_png = Image.open(BytesIO(img_data.content))
 
+
+# Read the png file, get to a single channel numpy array of zeroes and ones.
+plt.imshow(img_png)
 img_rgba = np.array(img_png)
-img_bw = img_rgba[:,:,0]
-img_bw[img_bw <= 120] = 1 
-img_bw[img_bw > 120] = 0
+img = img_rgba[:,:,0]
+plt.imshow(img)
 
-plt.imshow(img_bw)
-img = img_bw
-
-
-# Count length of verticals as a way of determining rotation
-verticals = []
-for c in range(img.shape[1]):
-    first_pixel_index = 0
-    last_pixel_index = 0
-    for r in range(img.shape[0]):
-        if img[r,c] == 1:
-            if first_pixel_index == 0:
-                first_pixel_index = r
-            last_pixel_index = r
-    vertical_length = last_pixel_index - first_pixel_index
-    verticals.append(vertical_length)
-
-print(verticals)
+# Trim whitespace from the canvas
+short = img[~np.all(img == 255, axis=1)]
+small = short.T[~np.all(short == 255, axis=0)].T
+img = small
 
 
-# Idea 1: get a vector from corner one to corner two. 
-#    Use that vector to change position of every pixel?
-#    Use that vector to just read things directly from QR?
+# To find a reasonable position for the code:
+# rotate a bit, trim whitespace, check width 
+# minimum width is a reasonable rotation
 
-# Idea 2: Rotate, check vertical lines again, rotate. 
-#    Finish when vertical lines are regular (near equal) in length
+#NOTE: this is iterative search, should at least be binary to be efficient
+#NOTE: this is exact to 1deg, could be a bit more (esp. if binary search)
+size_sums = []
+for angle in range(360):
+    rotated = interpolation.rotate(img, angle, cval=255)
+    clipped = clip_whitespace(rotated)
+    size_sums.append(clipped.shape[0])
 
-
-
-
-
-
-
-
+nice_angle = np.array(size_sums).argmin()
+rotated = interpolation.rotate(img, 58, cval=255)
+clipped = clip_whitespace(rotated)
+plt.imshow(clipped)
